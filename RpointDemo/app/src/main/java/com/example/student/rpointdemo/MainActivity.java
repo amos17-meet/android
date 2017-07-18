38330ddf122d0e5f389565bb8141102bd346c2eb
naim18-meet <naimjm@gmail.com>
2017-07-18 15:04:56 +0300

    beggining of the aplication, variabls, xml pages

--- RpointDemo/app/src/main/java/com/example/student/rpointdemo/MainActivity.java
+++ RpointDemo/app/src/main/java/com/example/student/rpointdemo/MainActivity.java
@@ -1,47 +1,24 @@
 package com.example.student.rpointdemo;
 
-import android.os.CountDownTimer;
-import android.os.PersistableBundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button contiuneButton;
-    private TextView textView;
-    private TextView bacView;
+
+    private Button mGenderButton;
+    private Button mAgeButton;
+    private Button mWeightButton;
+    private EditText mInputAge;
+    private EditText mInputWeight;
+    private EditText mInputGender;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.gender_page);
 
-        contiuneButton=(Button) findViewById(R.id.continu_button);
-        textView=(TextView) findViewById(R.id.text_view);
-
+        mGenderButton=(Button) findViewById(R.id.gender_button);
     }
-    public void onClick(View view){
-        new CountDownTimer(5000, 1000) {
-
-            public void onTick(long millisUntilFinished) {
-                textView.setText("continue blowing: " + millisUntilFinished / 1000);
-            }
-
-            public void onFinish() {
-                textView.setText("done!");
-                setContentView(R.layout.bac);
-
-                bacView=(TextView) findViewById(R.id.bac_view);
-                bacView.setText("hiiiiiii");
-
-            }
-        }.start();
-    }
-
-
 }
-
